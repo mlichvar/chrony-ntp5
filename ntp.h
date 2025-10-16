1cfe7d83e45f3aaaf7c9ae690436d351605dce0f
Miroslav Lichvar <mlichvar@redhat.com>
2025-10-16 16:22:47 +0200

    SQUASH: ntp: update draft ID to draft-ietf-ntp-ntpv5-07

--- ntp.h
+++ ntp.h
@@ -196,7 +196,7 @@ typedef struct {
   NTP_int64 mono_receive_ts;
 } NTP_EFMonoReceiveTs;
 
-#define NTP_EF_DRAFT_ID_STRING          "draft-ietf-ntp-ntpv5-06"
+#define NTP_EF_DRAFT_ID_STRING          "draft-ietf-ntp-ntpv5-07"
 
 /* Authentication extension fields */
 
